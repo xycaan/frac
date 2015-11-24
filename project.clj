f06e1c3437fa7e72ca692182a9c8c7ac72a5f6f1
xycaan <xycaan@gmail.com>
2015-11-24 22:01:39 +0000

    Remove complex.clj, add gui with seesaw for coord/scale selection

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [quil "2.2.6" :exclusions [org.clojure/clojure]]]
+                 [quil "2.2.6" :exclusions [org.clojure/clojure]]
+                 [seesaw "1.4.5"]]
   :main frac.core)
--- src/frac/core.clj
+++ src/frac/core.clj
@@ -1,52 +1,62 @@
 (ns frac.core
   (:require [quil.core :as q])
-  (:require [frac.complex :refer :all]))
+  (:require [frac.complex :refer :all])
+  (:require [seesaw.core :as s]))
 
-(def wi 250)
-(def he 250)
+(set! *unchecked-math* :warn-on-boxed)  
+(def draw? (atom false))  
+(def maxits 50)
 
-(def scale (atom (* 0.0008 he)))
-(def xoff (atom (- 0 (/ he 2))))
-(def yoff (atom (- 0 (/ he 2))))
-(def its 50)
+(s/native!)
+(def f (s/frame :title "Mandelbrot Generator"))
+(def xtext (s/text :text "0" :editable? true :columns 3))
+(def ytext (s/text :text "0" :editable? true :columns 3))
+(def rtext (s/text :text "0" :editable? true :columns 3))
+(def gbutton (s/button :text "GENERATE"))
 
-(defn mandelbrot [r i]
- (count (take its (take-while #(<= (modu %) 2) (iterate #( add (mul % %) [r i]) [r i])))))
 
-(defn gen []
+(defn mandelbrot [x y]
+ (loop [r x i y cnt 0]
+   (let [rsqr (* r r)
+         isqr (* i i)]
+     (if (or (>= cnt maxits) (>= (+ rsqr isqr) 4)) cnt 
+       (recur (+ (- rsqr isqr) x) (+ (* 2 i r) y) (inc cnt))))))
+
+(defn gen [cenx ceny r]
   (let [p (q/pixels)
         w (q/width)
-        h (q/height)]
+        h (q/height)
+        scalex (/ (* 2 r) w)
+        scaley (/ (* 2 r) h)
+        xlist (float-array (range (- cenx r) (+ cenx r) scalex))
+        ylist (float-array (range (- ceny r) (+ ceny r) scaley))]
     (doseq [x (range w) y (range h)]
-      (let [m (mandelbrot (* @scale (+ x @xoff)) (* @scale (+ y @yoff)))
-            c (if (= m its) 0 m)]
+      (let [m (mandelbrot (aget xlist x) (aget ylist y))
+            c (if (= m maxits) 0 m)]
         (aset p (+ x (* y w)) (q/color (* 1.5 c) (* 5.2 c) (* 3.8 c))))))
   (q/update-pixels))
 
+(defn parsetod [d]
+  (if (re-find #"^-?\d+\.?\d*$" d) (read-string d) 0))
+  
+
 (defn setup []
-  (gen))
+  ;(gen 0 0 2)
+  (s/config! f :content (s/left-right-split (s/left-right-split xtext ytext) (s/left-right-split rtext gbutton)) :resizable? false)
+  (->> f s/show! s/pack!)
+  (s/listen gbutton :mouse-clicked (fn [e] (if (= @draw? false) (swap! draw? #(not %))))))
 
-(defn draw [])
+(defn draw []
+  (if (= @draw? true) (do (swap! draw? #(not %)) (gen (parsetod (s/config xtext :text)) (parsetod (s/config ytext :text)) (let [g (parsetod (s/config rtext :text))] (if (= g 0) 1 g))))))
 
-(defn click []
-  (swap! xoff #(+ (q/mouse-x) (- (/ (q/width) 2)) %))
-  (swap! yoff #(+ (q/mouse-y) (- (/ (q/height) 2)) %)))
-
-(defn wheel [z] 
-  (swap! scale #(if (pos? z) (* 1.1 %) (* 0.9 %)))
-  (prn @scale))
-
-(defn keyp []
-  (gen))
+(defn click [])
 
 (q/defsketch example
-  :title "Mandel"
+  :title "Frac"
   :setup setup
   :draw draw
-  :size [wi he]
-  :mouse-clicked click
-  :mouse-wheel wheel
-  :key-pressed keyp)
+  :size [900 900]
+  :mouse-clicked click)
                 
 (defn -main [& args])
     
