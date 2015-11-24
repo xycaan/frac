042d788fa6e0c35fa52a27fed2d1d064f7311062
xycaan <xycaan@gmail.com>
2015-11-24 22:29:06 +0000

    Add type hinting for more performance

--- src/frac/core.clj
+++ src/frac/core.clj
@@ -3,7 +3,7 @@
   (:require [frac.complex :refer :all])
   (:require [seesaw.core :as s]))
 
-(set! *unchecked-math* :warn-on-boxed)  
+;(set! *unchecked-math* :warn-on-boxed)  
 (def draw? (atom false))  
 (def maxits 50)
 
@@ -15,25 +15,25 @@
 (def gbutton (s/button :text "GENERATE"))
 
 
-(defn mandelbrot [x y]
+(defn mandelbrot [^double x ^double y]
  (loop [r x i y cnt 0]
    (let [rsqr (* r r)
          isqr (* i i)]
-     (if (or (>= cnt maxits) (>= (+ rsqr isqr) 4)) cnt 
+     (if (or (>= cnt ^int maxits) (>= (+ rsqr isqr) 4)) cnt 
        (recur (+ (- rsqr isqr) x) (+ (* 2 i r) y) (inc cnt))))))
 
-(defn gen [cenx ceny r]
-  (let [p (q/pixels)
-        w (q/width)
-        h (q/height)
+(defn gen [^double cenx ^double ceny ^double r]
+  (let [^ints p (q/pixels)
+        ^int w (q/width)
+        ^int h (q/height)
         scalex (/ (* 2 r) w)
         scaley (/ (* 2 r) h)
         xlist (float-array (range (- cenx r) (+ cenx r) scalex))
         ylist (float-array (range (- ceny r) (+ ceny r) scaley))]
-    (doseq [x (range w) y (range h)]
+    (doseq [^int x (range w) ^int y (range h)]
       (let [m (mandelbrot (aget xlist x) (aget ylist y))
-            c (if (= m maxits) 0 m)]
-        (aset p (+ x (* y w)) (q/color (* 1.5 c) (* 5.2 c) (* 3.8 c))))))
+            ^int c (if (= m maxits) 0 m)]
+        (aset p (+ x (* y w)) (q/color ^double (* 1.5 c) ^double (* 5.2 c) ^double (* 3.8 c))))))
   (q/update-pixels))
 
 (defn parsetod [d]
